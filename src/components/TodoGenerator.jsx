9c596f5a8836f884f49a14efe1652f00278c4841
monsterleon <1115209655@qq.com>
2022-08-02 12:58:46 +0800

    refactor: modify addTodoList

--- src/components/TodoGenerator.jsx
+++ src/components/TodoGenerator.jsx
@@ -1,16 +1,21 @@
 import "../css/TodoGenerator.css";
+import {useState} from "react";
 function TodoGenerator(props) {
   const { updateTodoItemList } = props;
+  const [inputValue, setInputValue] = useState('');
   const addTodoList = function() {
-    const value = document.getElementById("input-text").value;
-    if (value !== "") {
-      updateTodoItemList(value);
-      document.getElementById("input-text").value = "";
+    if(inputValue!==""){
+      updateTodoItemList(inputValue);
+      document.getElementById("input-text").value = ""
     }
   };
+  const handleChange = function(event){
+    const value = event.target.value;
+    setInputValue(value)
+  }
   return (
     <div className="todo-generator-box">
-      <input type="text" id="input-text" />
+      <input type="text" id="input-text" onChange={handleChange} />
       <button className="add-button" onClick={addTodoList}>
         add
       </button>
