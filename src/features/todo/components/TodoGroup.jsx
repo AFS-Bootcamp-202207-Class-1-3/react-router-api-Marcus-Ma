c681e63d65ed092665f7fdedf4d22ce4d584aa0a
monsterleon <1115209655@qq.com>
2022-08-05 10:32:15 +0800

    refactor: move modal to TodoGroup

--- src/features/todo/components/TodoGroup.jsx
+++ src/features/todo/components/TodoGroup.jsx
@@ -1,32 +1,77 @@
 import "../css/TodoGroup.css";
 import TodoItem from "./TodoItem";
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { getTodos } from "../../apis/todoApi.js";
-import { setTodoList } from "../todoSlice";
-import { List } from 'antd';
+import { getTodos,changeTodoContext } from "../../apis/todoApi.js";
+import { setTodoList,updateTodoContext } from "../todoSlice";
+import { List,Modal,message } from 'antd';
 function TodoGroup() {
   const todos = useSelector(state => state.todo.todos);
   const dispatch = useDispatch();
+  const errorMessage = "修改内容不能为空!";
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [updateValue, setUpdateValue] = useState("");
+  const [nowTodo,setNowTodo] = useState({});
   // [dispatch] 解决依赖
   useEffect(() => {
     getTodos().then(response => {
       dispatch(setTodoList(response.data));
     })
   }, [dispatch]);
-  const noDoneTodos = todos
-    .filter(todo => todo.done === false);
+  const noDoneTodos = todos.filter(todo => todo.done === false);
+
+  // 修改
+    const showModal = function(todo){
+      setNowTodo(todo);
+      setUpdateValue(todo.context);
+      setIsModalVisible(true);
+    };
+    const error = () => {
+      message.error(errorMessage);
+    };
+    const handleOk = function(todo){
+      if (updateValue === "") {
+        error();
+      } else {
+        changeTodoContext(todo.id, { context: updateValue }).then(response => {
+          dispatch(updateTodoContext(response.data));
+          setIsModalVisible(false);
+        });
+      }
+    };
+    const handleCancel = () => {
+      setIsModalVisible(false);
+    };
+    const handleUpdateValue = event => {
+      setUpdateValue(event.target.value);
+    };
   return (
     <div className="todo-group-box">
-       <List
+        <List
           bordered
           dataSource={noDoneTodos}
           renderItem={item => (
             <List.Item>
-              <TodoItem key={item.id} todo={item} />
+              <TodoItem key={item.id} todo={item} showModal={showModal} />
             </List.Item>
           )}
         />
+        <Modal
+          title="Update Context"
+          visible={isModalVisible}
+          onOk={()=>handleOk(nowTodo)}
+          onCancel={handleCancel}
+        >
+          <textarea
+            value={updateValue}
+            cols="25"
+            rows="5"
+            className="my-text"
+            onChange={handleUpdateValue}
+          >
+            {updateValue}
+          </textarea>
+        </Modal>
     </div>
   );
 }
--- src/features/todo/components/TodoItem.jsx
+++ src/features/todo/components/TodoItem.jsx
@@ -1,49 +1,23 @@
 import "../css/TodoItem.css";
-import { Button, Modal, message } from "antd";
+import { Button, message } from "antd";
 import { EditFilled, DeleteFilled } from "@ant-design/icons";
-import { useState } from "react";
 import { useDispatch } from "react-redux";
 import {
   deleteTodoItem,
   changeDoneState,
-  updateTodoContext
 } from "../todoSlice";
 import {
   deleteTodoById,
   changeTodoDone,
-  changeTodoContext
 } from "../../apis/todoApi";
 function TodoItem(props) {
-  const { todo } = props;
+  const { todo,showModal } = props;
   const dispatch = useDispatch();
-  const errorMessage = "修改内容不能为空!";
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const [updateValue, setUpdateValue] = useState(todo.context);
-  const showModal = () => {
-    setIsModalVisible(true);
-  };
-  const error = () => {
-    message.error(errorMessage);
-  };
+
   const success = (successMsg) => {
     message.success(successMsg);
   };
-  const handleOk = () => {
-    if (updateValue === "") {
-      error();
-    } else {
-      changeTodoContext(todo.id, { context: updateValue }).then(response => {
-        dispatch(updateTodoContext(response.data));
-        setIsModalVisible(false);
-      });
-    }
-  };
-  const handleCancel = () => {
-    setIsModalVisible(false);
-  };
-  const handleUpdateValue = event => {
-    setUpdateValue(event.target.value);
-  };
+
   const deleteTodo = function() {
     deleteTodoById(todo.id).then(response => {
       dispatch(deleteTodoItem(todo.id));
@@ -69,7 +43,7 @@ function TodoItem(props) {
         <Button
           type="primary"
           icon={<EditFilled />}
-          onClick={showModal}
+          onClick={(e)=>showModal(todo)}
           ghost
         />
         <Button
@@ -80,23 +54,6 @@ function TodoItem(props) {
           onClick={deleteTodo}
         />
       </div>
-      {/* 往上提一层 */}
-      <Modal
-        title="Update Context"
-        visible={isModalVisible}
-        onOk={handleOk}
-        onCancel={handleCancel}
-      >
-        <textarea
-          value={updateValue}
-          cols="25"
-          rows="5"
-          className="my-text"
-          onChange={handleUpdateValue}
-        >
-          {updateValue}
-        </textarea>
-      </Modal>
     </div>
   );
 }
